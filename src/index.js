1bf7d19d2b50085b6cca14069590225de78cf46d
Cain Atkinson <yellowsink@protonmail.com>
2022-04-23 16:26:38 +0100

    Revert "fix a compat issue"
    This reverts commit 8ba9448c527a8735d2104550120f50b4971608d6.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ export { default as boundCumcache } from "./bound-cumcache";
 export { default as findByDomNode } from "./findByDomNode";
 export { default as depend } from "./depend";
 
-import * as settings from "./settings";
-export { settings };
+export * as settings from "./settings";
 export * from "./settings";
